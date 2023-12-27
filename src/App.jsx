305cb1aacca5bd1e2fbbcecae83218b04d98025e
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2023-12-27 15:36:39 -0300

    Create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
+import styles from './App.module.css';
 import './global.css'
+
 
 export function App() {
   return (
     <div>
       <Header />
 
-    <Post 
-      author="Fernando Pimenta" 
-      content="Ignite, o melhor curso de todos os tempos!"
-    />
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+          <Post 
+          author="Fernando Pimenta" 
+          content="Ignite, o melhor curso de todos os tempos!"
+        />
+      </main>
+    </div>
+
 
     </div>
 
