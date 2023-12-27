d98247560e75fa0cb24757f4e060d38408df97a1
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2023-12-27 16:49:33 -0300

    Add textarea and button to publish comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,6 +25,17 @@ export function Post() {
                     <a href="">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder="comentar" />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+                
+            </form>
         </article>
     )
 }
